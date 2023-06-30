fd74e96ca1938a6b8816100319c2ac1bae098487
Rashmi Jha <rashmi474012@gmail.com>
2023-06-30 14:58:58 +0530

    make partials and layout

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
+
+//use express-ejs-layout
+app.use(expressEjsLayouts);
 
 //use express router
 app.use('/', require('./routes/index'));
