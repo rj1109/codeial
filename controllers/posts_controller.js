00c6331cc9554fa9580175c14dcbcb1e6e3c97a7
rj1109 <20it1048@mitsgwl.ac.in>
2023-07-08 11:49:19 +0530

    store post content in database

---
+++ controllers/posts_controller.js
@@ -0,0 +1,14 @@
+const Post = require('../models/post');
+
+module.exports.create =async function(req, res){
+    try{
+        const post = await Post.create({
+            content:req.body.content,
+            user: req.user._id
+        });
+
+        return res.redirect('back');
+    }catch(err){
+        console.log("Error in pushing post data to database");
+    }
+}
