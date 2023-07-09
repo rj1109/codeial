6d4f256d3253b8557de0b96fe5ebac225cb91495
rj1109 <20it1048@mitsgwl.ac.in>
2023-07-09 19:46:52 +0530

    delete a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create =async function(req, res){
     try{
@@ -10,5 +11,21 @@ module.exports.create =async function(req, res){
         return res.redirect('back');
     }catch(err){
         console.log("Error in pushing post data to database",err);
+    }
+}
+
+module.exports.destroy = async function(req, res){
+    try{
+        const post =await Post.findById(req.params.id);
+
+        if(post && post.user == req.user.id){
+            await Post.deleteOne({_id: post._id});
+            await Comment.deleteMany({post: req.params.id});
+            return res.redirect('back');
+        }else{
+            res.redirect('back');
+        }
+    }catch(err){
+        console.log('Error in deleting the post in post controller', err);
     }
 }
