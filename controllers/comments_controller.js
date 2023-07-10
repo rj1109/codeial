25bacb4059265c91c8e6d5daba06902475d8acb0
rj1109 <20it1048@mitsgwl.ac.in>
2023-07-10 22:04:15 +0530

    delete authorized comment and distribute code in partials

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,3 +24,34 @@ module.exports.create = async function(req, res) {
     res.status(500).json({ error: 'Internal server error' });
   }
 };
+
+module.exports.destroy = async function(req, res) {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      return res.status(404).json({ error: 'Comment not found' });
+    }
+
+    let postId = comment.post;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    if (comment.user.toString() === req.user._id.toString() || post.user.toString() === req.user._id.toString()) {
+      // User is the creator of the comment or the post
+      await Comment.findByIdAndDelete(req.params.id);
+      post.comments.pull(req.params.id);
+      await post.save();
+      return res.redirect('back');
+    } else {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
